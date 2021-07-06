79f7c1523b0fcee2b756adce031e4e2caa10ecab
Karina Johansson <kahjohansson@gmail.com>
2021-07-06 18:55:38 -0300

    update init file

--- pyhtmm/__init__.py
+++ pyhtmm/__init__.py
@@ -2,6 +2,9 @@ from .htmm import HTMM, EM
 from .document import _Document
 from .sentence import _Sentence
 from .utils import *
+from .process import *
+from .fast_restricted_hmm import *
+from .fast_restricted_viterbi import *
 
 # __all__ = [
 #     'document',
